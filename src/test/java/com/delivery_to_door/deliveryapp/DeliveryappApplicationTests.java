a9944b956518ad6eca22e15cc155638687b158a6
caneel7 <caneelmacwan98@gmail.com>
2024-10-24 21:35:08 +0530

    initial commit : connected to db

---
+++ src/test/java/com/delivery_to_door/deliveryapp/DeliveryappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delivery_to_door.deliveryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
